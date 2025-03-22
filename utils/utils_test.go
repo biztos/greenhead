fead8c226692e5052736b902d103e64d8e986669
Kevin Frost <biztos@mac.com>
2025-03-23 00:41:11 +0700

    utils test 100%, backfill when braindead…

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -3,12 +3,99 @@ package utils_test
 import (
 	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/biztos/greenhead/runner"
 	"github.com/biztos/greenhead/utils"
 )
+
+type CanNotMarshalJson struct {
+	Bar func()
+}
+
+func TestMustJsonStringOK(t *testing.T) {
+
+	require := require.New(t)
+	v := map[string]any{"foo": "bar", "bar": []int{1, 2, 3}}
+	var s string
+	require.NotPanics(func() { s = utils.MustJsonString(v) }, "not panic")
+	exp := `{"bar":[1,2,3],"foo":"bar"}` // uh-oh, any order guarantee?
+	require.Equal(exp, s, "json as expected")
+}
+
+func TestMustJsonStringPanics(t *testing.T) {
+
+	require := require.New(t)
+
+	v := map[string]any{"foo": &CanNotMarshalJson{}}
+	require.PanicsWithValue("json: unsupported type: func()",
+		func() { utils.MustJsonString(v) }, "panic")
+
+}
+
+func TestMustJsonStringPrettyOK(t *testing.T) {
+
+	require := require.New(t)
+	v := map[string]any{"foo": "bar", "bar": []int{1, 2, 3}}
+	var s string
+	require.NotPanics(func() { s = utils.MustJsonStringPretty(v) }, "not panic")
+	exp := `{
+  "bar": [
+    1,
+    2,
+    3
+  ],
+  "foo": "bar"
+}`
+	require.Equal(exp, s, "json as expected")
+}
+
+func TestMustJsonStringPrettyPanics(t *testing.T) {
+
+	require := require.New(t)
+
+	v := map[string]any{"foo": &CanNotMarshalJson{}}
+	require.PanicsWithValue("json: unsupported type: func()",
+		func() { utils.MustJsonStringPretty(v) }, "panic")
+
+}
+
+func TestDurOk(t *testing.T) {
+
+	require := require.New(t)
+
+	s := utils.Dur(time.Now())
+	d, err := time.ParseDuration(s)
+	require.NoError(err)
+
+	// A bit dumb here, but let's just say it should not take a millisec!
+	require.NotZero(d)
+	require.True(d < time.Millisecond, "less than one millisecond")
+
+}
+
+func TestDurLogOk(t *testing.T) {
+
+	require := require.New(t)
+
+	a := utils.DurLog(time.Now())
+	require.Equal(2, len(a), "has two elements")
+
+	label, ok := a[0].(string)
+	require.True(ok, "first is string")
+	require.Equal(label, "duration")
+
+	s, ok := a[1].(string)
+	require.True(ok, "second is string")
+	// As above...
+	d, err := time.ParseDuration(s)
+	require.NoError(err)
+	require.NotZero(d)
+	require.True(d < time.Millisecond, "less than one millisecond")
+
+}
 
 func TestUnmarshalFileErrBadExt(t *testing.T) {
 
