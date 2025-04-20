627906b766d6790f53619270ee3d5ae8b2774495
Kevin Frost <biztos@mac.com>
2025-04-21 00:34:41 +0700

    load all tools centrally

--- cmd/ghd/main.go
+++ cmd/ghd/main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"github.com/biztos/greenhead"
 
-	// Load the demo tools here.
-	_ "github.com/biztos/greenhead/tools/demo"
+	_ "github.com/biztos/greenhead/tools/all"
 )
 
 func main() {
